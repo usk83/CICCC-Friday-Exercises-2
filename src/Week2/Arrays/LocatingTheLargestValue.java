1b3efbcf6c3ed50fb8c6175616838211b04798ab
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2019-10-25 14:51:11 -0700

    Solve 149 Locating the Largest Value in an Array
    149 Arrays Locating the Largest Value in an Array
    http://programmingbydoing.com/a/locating-the-largest-value-in-an-array.h
    tml

--- src/Week2/Arrays/LocatingTheLargestValue.java
+++ src/Week2/Arrays/LocatingTheLargestValue.java
@@ -1,8 +1,36 @@
 // http://programmingbydoing.com/a/locating-the-largest-value-in-an-array.html
 package Week2.Arrays;
 
-public class LocatingTheLargestValue {
-  public static void main(String[] args) {
+import java.util.Random;
 
+public class LocatingTheLargestValue {
+  private final static int ARRAY_LENGTH = 10;
+  private final static Random RAND = new Random();
+
+  public static void main(String[] args) {
+    int[] arr = new int[ARRAY_LENGTH];
+    for (int i = 0; i < ARRAY_LENGTH; i++) {
+      arr[i] = RAND.nextInt(100) + 1;
+    }
+
+    printIntArray(arr, "Array");
+
+    int maxPosition = 0;
+    for (int i = 1; i < ARRAY_LENGTH; i++) {
+      if (arr[maxPosition] < arr[i]) {
+        maxPosition = i;
+      }
+    }
+
+    System.out.printf("\nThe largest value is %d\n", arr[maxPosition]);
+    System.out.printf("It is in slot %d\n", maxPosition);
+  }
+
+  private static void printIntArray(int[] arr, String name) {
+    System.out.printf("%s:", name);
+    for (int num: arr) {
+      System.out.printf(" %d", num);
+    }
+    System.out.println();
   }
 }
