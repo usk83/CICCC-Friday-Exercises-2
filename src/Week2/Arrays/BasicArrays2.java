21e6ecf64a970263d05e8fa32b68eb54325cad07
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2019-10-25 13:52:52 -0700

    Solve 140 Basic Arrays 2
    140 Arrays Basic Arrays 2
    http://programmingbydoing.com/a/basic-arrays-2.html

--- src/Week2/Arrays/BasicArrays2.java
+++ src/Week2/Arrays/BasicArrays2.java
@@ -1,8 +1,31 @@
 // http://programmingbydoing.com/a/basic-arrays-2.html
 package Week2.Arrays;
 
-public class BasicArrays2 {
-  public static void main(String[] args) {
+import java.util.Random;
 
+public class BasicArrays2 {
+  private final static Random RAND = new Random();
+
+  public static void main(String[] args) {
+    int[] arr = {
+      getRandomNumber1to100(),
+      getRandomNumber1to100(),
+      getRandomNumber1to100(),
+      getRandomNumber1to100(),
+      getRandomNumber1to100(),
+      getRandomNumber1to100(),
+      getRandomNumber1to100(),
+      getRandomNumber1to100(),
+      getRandomNumber1to100(),
+      getRandomNumber1to100()
+    };
+
+    for (int i = 0; i < arr.length; i++) {
+      System.out.printf("Slot %d contains a %d\n", i, arr[i]);
+    }
+  }
+
+  private static int getRandomNumber1to100() {
+    return RAND.nextInt(100) + 1;
   }
 }
