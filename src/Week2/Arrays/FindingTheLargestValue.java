81c3e417198aa619185b82f4146b761463e27699
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2019-10-25 14:48:30 -0700

    Solve 148 Finding the Largest Value in an Array
    148 Arrays Finding the Largest Value in an Array
    http://programmingbydoing.com/a/finding-the-largest-value-in-an-array.ht
    ml

--- src/Week2/Arrays/FindingTheLargestValue.java
+++ src/Week2/Arrays/FindingTheLargestValue.java
@@ -1,8 +1,35 @@
 // http://programmingbydoing.com/a/finding-the-largest-value-in-an-array.html
 package Week2.Arrays;
 
-public class FindingTheLargestValue {
-  public static void main(String[] args) {
+import java.util.Random;
 
+public class FindingTheLargestValue {
+  private final static int ARRAY_LENGTH = 10;
+  private final static Random RAND = new Random();
+
+  public static void main(String[] args) {
+    int[] arr = new int[ARRAY_LENGTH];
+    for (int i = 0; i < ARRAY_LENGTH; i++) {
+      arr[i] = RAND.nextInt(100) + 1;
+    }
+
+    printIntArray(arr, "Array");
+
+    int max = arr[0];
+    for (int i = 1; i < ARRAY_LENGTH; i++) {
+      if (max < arr[i]) {
+        max = arr[i];
+      }
+    }
+
+    System.out.printf("\nThe largest value is %d\n", max);
+  }
+
+  private static void printIntArray(int[] arr, String name) {
+    System.out.printf("%s:", name);
+    for (int num: arr) {
+      System.out.printf(" %d", num);
+    }
+    System.out.println();
   }
 }
