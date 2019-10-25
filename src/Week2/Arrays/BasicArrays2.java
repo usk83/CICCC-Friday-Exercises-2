8a1be21e96bf774e73702a8f0f8b93ceb570b000
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2019-10-25 13:57:03 -0700

    Update 140 Basic Arrays2

--- src/Week2/Arrays/BasicArrays2.java
+++ src/Week2/Arrays/BasicArrays2.java
@@ -4,28 +4,18 @@ package Week2.Arrays;
 import java.util.Random;
 
 public class BasicArrays2 {
+  private final static int ARRAY_LENGTH= 10;
   private final static Random RAND = new Random();
 
   public static void main(String[] args) {
-    int[] arr = {
-      getRandomNumber1to100(),
-      getRandomNumber1to100(),
-      getRandomNumber1to100(),
-      getRandomNumber1to100(),
-      getRandomNumber1to100(),
-      getRandomNumber1to100(),
-      getRandomNumber1to100(),
-      getRandomNumber1to100(),
-      getRandomNumber1to100(),
-      getRandomNumber1to100()
-    };
+    int[] arr = new int[ARRAY_LENGTH];
 
-    for (int i = 0; i < arr.length; i++) {
+    for (int i = 0; i < ARRAY_LENGTH; i++) {
+      arr[i] = RAND.nextInt(100) + 1;
+    }
+
+    for (int i = 0; i < ARRAY_LENGTH; i++) {
       System.out.printf("Slot %d contains a %d\n", i, arr[i]);
     }
-  }
-
-  private static int getRandomNumber1to100() {
-    return RAND.nextInt(100) + 1;
   }
 }
