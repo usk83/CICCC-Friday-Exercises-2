a008aca2aea7eef6bdcf434e167650d5b2449606
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2019-10-25 15:01:18 -0700

    Solve 179 Basic ArrayLists 2
    179 ArrayLists Basic ArrayLists 2
    http://programmingbydoing.com/a/basic-arraylists-2.html

--- src/Week2/ArrayLists/BasicArrayLists2.java
+++ src/Week2/ArrayLists/BasicArrayLists2.java
@@ -1,8 +1,22 @@
 // http://programmingbydoing.com/a/basic-arraylists-2.html
 package Week2.ArrayLists;
 
-public class BasicArrayLists2 {
-  public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class BasicArrayLists2 {
+  private final static int ARRAY_LENGTH = 10;
+  private final static Random RAND = new Random();
+
+  public static void main(String[] args) {
+    ArrayList<Integer> arrayList = new ArrayList<>(ARRAY_LENGTH);
+    for(int i = ARRAY_LENGTH; i > 0; i--) {
+      arrayList.add(RAND.nextInt(100) + 1);
+    }
+    printArrayList(arrayList, "ArrayList");
+  }
+
+  private static void printArrayList(ArrayList<?> arrayList, String name) {
+    System.out.printf( "%s: %s\n", name, arrayList );
   }
 }
