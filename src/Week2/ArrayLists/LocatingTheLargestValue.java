0392614554ff758d7c12621a9eefa2864b9256ef
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2019-10-25 15:26:45 -0700

    Solve 186 Locating the Largest Value in an ArrayList
    186 ArrayLists Locating the Largest Value in an ArrayList
    http://programmingbydoing.com/a/locating-the-largest-value-in-an-arrayli
    st.html

--- src/Week2/ArrayLists/LocatingTheLargestValue.java
+++ src/Week2/ArrayLists/LocatingTheLargestValue.java
@@ -1,8 +1,36 @@
 // http://programmingbydoing.com/a/locating-the-largest-value-in-an-arraylist.html
 package Week2.ArrayLists;
 
-public class LocatingTheLargestValue {
-  public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class LocatingTheLargestValue {
+  private final static int ARRAY_LENGTH = 10;
+  private final static int MAX_NUMBER = 100;
+  private final static Random RAND = new Random();
+
+  public static void main(String[] args) {
+    ArrayList<Integer> arrayList = new ArrayList<>(ARRAY_LENGTH);
+    for(int i = ARRAY_LENGTH; i > 0; i--) {
+      arrayList.add(RAND.nextInt(MAX_NUMBER) + 1);
+    }
+    printArrayList(arrayList, "ArrayList");
+
+    int maxPosition = 0;
+    int max = arrayList.get(maxPosition);
+    int size = arrayList.size(); // actually, it's ARRAY_LENGTH
+    for (int i = 1; i < size; i++) {
+      int current = arrayList.get(i);
+      if (max < current) {
+        maxPosition = i;
+        max = current;
+      }
+    }
+
+    System.out.printf("\nThe largest value is %d, which is in slot %d\n", max, maxPosition);
+  }
+
+  private static void printArrayList(ArrayList<?> arrayList, String name) {
+    System.out.printf( "%s: %s\n", name, arrayList );
   }
 }
