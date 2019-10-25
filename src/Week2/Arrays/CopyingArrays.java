d7f17beb2e0090ad9f65c891393eb1df4f0647f5
Yusuke Takahashi <usk@sfc.wide.ad.jp>
2019-10-25 14:02:04 -0700

    Solve 142 Copying Arrays
    142 Arrays Copying Arrays
    http://programmingbydoing.com/a/copying-arrays.html

--- src/Week2/Arrays/CopyingArrays.java
+++ src/Week2/Arrays/CopyingArrays.java
@@ -1,8 +1,33 @@
 // http://programmingbydoing.com/a/copying-arrays.html
 package Week2.Arrays;
 
-public class CopyingArrays {
-  public static void main(String[] args) {
+import java.util.Random;
 
+public class CopyingArrays {
+  private final static int ARRAY_LENGTH= 10;
+  private final static Random RAND = new Random();
+
+  public static void main(String[] args) {
+    int[] arr1 = new int[ARRAY_LENGTH];
+    for (int i = 0; i < ARRAY_LENGTH; i++) {
+      arr1[i] = RAND.nextInt(100) + 1;
+    }
+    int[] arr2 = new int[ARRAY_LENGTH];
+    for (int i = 0; i < ARRAY_LENGTH; i++) {
+      arr2[i] = arr1[i];
+    }
+
+    arr1[ARRAY_LENGTH - 1] = -7;
+
+    printIntArray(arr1, "Array 1");
+    printIntArray(arr2, "Array 2");
+  }
+
+  private static void printIntArray(int[] arr, String name) {
+    System.out.printf("%s:", name);
+    for (int num: arr) {
+      System.out.printf(" %d", num);
+    }
+    System.out.println();
   }
 }
